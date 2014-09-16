redhat-cip/config-tools

commit 2ba45ba85fafec0b17dd9c74abc2f48e9b7031f1

download.sh: also copy -*rev in /etc/config-tools
Include puppet & serverspec ref in /etc/config-tools on install-server.
Will be more easier to catch the git rev.

Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

download.sh
@@ -224,7 +224,7 @@ fi
 
 update_or_clone "$puppetgit" puppet-module
 
-git --git-dir=puppet-module/.git rev-parse HEAD > puppet-module-rev
+git --git-dir=puppet-module/.git rev-parse HEAD > $TOP/etc/config-tools/puppet-module-rev
 
 if [ -n "$tag" -a "$tagged" = 1 ]; then
     sed -i -e "s/master/$(cat puppet-module-rev)/" ./puppet-module/Puppetfile
@@ -242,7 +242,7 @@ cp -a modules $TOP/etc/puppet/
 
 update_or_clone "$serverspecgit" serverspec
 
-git --git-dir=serverspec/.git rev-parse HEAD > serverspec-rev
+git --git-dir=serverspec/.git rev-parse HEAD > $TOP/etc/config-tools/serverspec-rev
 
 cp infra/arch.yml.tmpl serverspec/
 sed -i "s/root/$USER/" serverspec/spec/spec_helper.rb

commit 98ff966cbc415a5fc6a7afc92aeafba97b97f848

Fix error introduced by 2ba45ba85fafec0b17dd9c74abc2f48e9b7031f1
When you launch provision.sh this script stop on r10k error.

  [R10K::TaskRunner - ERROR] Task #<R10K::Task::Module::Sync:0x00000002e2d720> failed while running:  at /home/gael/.r10k/git/git---github.com-enovance-puppet-openstack-cloud.git. (Does the remote repository need to be updated?)

The puppet-module/Puppetfile file seems not good :

```
mod 'cloud',
  :git => 'git://github.com/enovance/puppet-openstack-cloud.git',
  :ref => ''
```

The problem is :

```
+ git --git-dir=puppet-module/.git rev-parse HEAD
+ '[' -n I.1.1.0 -a 1 = 1 ']'
++ cat puppet-module-rev
cat: puppet-module-rev: Aucun fichier ou dossier de ce type
+ sed -i -e s/master// ./puppet-module/Puppetfile
```

The puppet-module-rev file has been moved by the following commit :

  * https://github.com/enovance/config-tools/commit/2ba45ba85fafec0b17dd9c74abc2f48e9b7031f1

download.sh
@@ -251,7 +251,7 @@ update_or_clone "$puppetgit" puppet-module
 git --git-dir=puppet-module/.git rev-parse HEAD > $TOP/etc/config-tools/puppet-module-rev
 
 if [ -n "$tag" -a "$tagged" = 1 ]; then
-    sed -i -e "s/master/$(cat puppet-module-rev)/" ./puppet-module/Puppetfile
+    sed -i -e "s/master/$(cat ${TOP}/etc/config-tools/puppet-module-rev)/" ./puppet-module/Puppetfile
 fi
 
 if [ "$LOCAL" != 1 ]; then