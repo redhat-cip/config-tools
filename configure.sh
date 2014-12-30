db12737387c6dab8d8aa21dd682fa25fb920f623
Emilien Macchi <emilien.macchi@enovance.com>
2014-12-29 19:07:02 -0500

    configure: kill puppet agent at each step
    It happens Puppet Agent is already running and it blocks the loop
    because there is no return.
    So this patch aims to ensure Puppet Agent is not running before running
    it.

--- configure.sh
+++ configure.sh
@@ -241,14 +241,18 @@ for (( step=$STEP; step<=$LAST; step++)); do # Yep, this is a bashism
             echo "Run Puppet on $h node (step ${step}, try $loop):"
             if run_parallel $step; then
                 if [ $h = $(hostname -s) ]; then
+                    pkill -9 puppet || true
                     puppet agent $PUPPETOPTS > $LOGDIR/$h.step${step}.try${loop}.log 2>&1 &
                 else
+                    ssh $SSHOPTS $USER@$h sudo -i pkill -9 puppet || true
                     ssh $SSHOPTS $USER@$h sudo -i puppet agent $PUPPETOPTS > $LOGDIR/$h.step${step}.try${loop}.log 2>&1 &
                 fi
             else
                 if [ $h = $(hostname -s) ]; then
+                    pkill -9 puppet || true
                     puppet agent $PUPPETOPTS 2>&1 | tee $LOGDIR/$h.step${step}.try${loop}.log
                 else
+                    ssh $SSHOPTS $USER@$h sudo -i pkill -9 puppet || true
                     ssh $SSHOPTS $USER@$h sudo -i puppet agent $PUPPETOPTS 2>&1 | tee $LOGDIR/$h.step${step}.try${loop}.log
                 fi
             fi
