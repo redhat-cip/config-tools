6f4c2b22572ccdc85bf44d9381cb3ca0f2615458
Frederic Lepied <frederic.lepied@enovance.com>
2015-01-09 21:57:01 +0100

    remove duplicated code with the hardware module and use it instead

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+# Copyright (C) 2014-2015 eNovance SAS <licensing@enovance.com>
 #
 # Author: Frederic Lepied <frederic.lepied@enovance.com>
 #
@@ -19,14 +19,11 @@
 '''Module to generate templates according to yaml variables.
 '''
 
-from merge import merge
-
-from itertools import izip
 from jinja2 import Template
 import os
-import re
-import types
 from yaml import dump, load, Loader
+
+from hardware import generate
 
 
 class Invalid(Exception):
@@ -138,159 +135,14 @@ def validate(variables):
     return True
 
 
-def _generate_range(num_range):
-    'Generate number for range specified like 10-12:20-30.'
-    for rang in num_range.split(':'):
-        boundaries = rang.split('-')
-        if len(boundaries) == 2:
-            try:
-                if boundaries[0][0] == '0':
-                    fmt = '%%0%dd' % len(boundaries[0])
-                else:
-                    fmt = '%d'
-                start = int(boundaries[0])
-                stop = int(boundaries[1]) + 1
-                if stop > start:
-                    step = 1
-                else:
-                    step = -1
-                    stop = stop - 2
-                for res in range(start, stop, step):
-                    yield fmt % res
-            except ValueError:
-                yield num_range
-        else:
-            yield num_range
-
-
-_RANGE_REGEXP = re.compile(r'^(.*?)([0-9]+-[0-9]+(:([0-9]+-[0-9]+))*)(.*)$')
-_IPV4_RANGE_REGEXP = re.compile(r'^[0-9:\-.]+$')
-
-
-def _generate_values(pattern):
-    '''Create a generator for ranges of IPv4 or names with ranges
-defined like 10-12:15-18 or from a list of entries.'''
-    if isinstance(pattern, list) or isinstance(pattern, tuple):
-        for elt in pattern:
-            yield elt
-    elif isinstance(pattern, dict):
-        for key, entry in pattern.items():
-            if key[0] == '=':
-                pattern[key[1:]] = _generate_values(entry)
-                del pattern[key]
-            else:
-                pattern[key] = entry
-        while True:
-            yield pattern
-    elif isinstance(pattern, str):
-        parts = pattern.split('.')
-        if _IPV4_RANGE_REGEXP.search(pattern) and \
-                len(parts) == 4 and (pattern.find(':') != -1 or
-                                     pattern.find('-') != -1):
-            gens = [_generate_range(part) for part in parts]
-            for part0 in gens[0]:
-                for part1 in gens[1]:
-                    for part2 in gens[2]:
-                        for part3 in gens[3]:
-                            yield '.'.join((part0, part1, part2, part3))
-                        gens[3] = _generate_range(parts[3])
-                    gens[2] = _generate_range(parts[2])
-                gens[1] = _generate_range(parts[1])
-        else:
-            res = _RANGE_REGEXP.search(pattern)
-            if res:
-                head = res.group(1)
-                foot = res.group(res.lastindex)
-                for num in _generate_range(res.group(2)):
-                    yield head + num + foot
-            else:
-                for _ in xrange(16387064):
-                    yield pattern
-    else:
-        for _ in xrange(16387064):
-            yield pattern
-
-
-STRING_TYPE = type('')
-GENERATOR_TYPE = types.GeneratorType
-
-
-def _call_nexts(model):
-    'Walk through the model to call next() on all generators.'
-    entry = {}
-    generated = False
-    for key in model.keys():
-        if isinstance(model[key], GENERATOR_TYPE):
-            entry[key] = model[key].next()
-            generated = True
-        elif isinstance(model[key], dict):
-            entry[key] = _call_nexts(model[key])
-        else:
-            entry[key] = model[key]
-    # We can have nested generators so call again
-    if generated:
-        return _call_nexts(entry)
-    else:
-        return entry
-
-
-def generate_list(model):
-    '''Generate a list of dict according to a model with ranges in
-values like host10-12 or 192.168.2.10-12:14-20.'''
-    # Safe guard for models without ranges
-    for value in model.values():
-        if type(value) != STRING_TYPE:
-            break
-        elif _RANGE_REGEXP.search(value):
-            break
-    else:
-        return [model]
-    # The model has a range starting from here
-    result = []
-    yielded = {}
-    yielded.update(model)
-    for key, value in yielded.items():
-        if key[0] == '=':
-            yielded[key[1:]] = _generate_values(value)
-            del yielded[key]
-        else:
-            yielded[key] = value
-    while True:
-        try:
-            result.append(_call_nexts(yielded))
-        except StopIteration:
-            break
-    return result
-
-
-def generate_dict(model):
-    '''Generate a dict with ranges in keys and values.'''
-    result = {}
-    for thekey in model.keys():
-        if thekey[0] == '=':
-            key = thekey[1:]
-            for newkey, val in izip(list(_generate_values(key)),
-                                    generate_list(model[thekey])):
-                try:
-                    result[newkey] = merge(result[key], val)
-                except KeyError:
-                    result[newkey] = val
-        else:
-            key = thekey
-            try:
-                result[key] = merge(result[key], model[key])
-            except KeyError:
-                result[key] = model[key]
-    return result
-
-
 def expand_template(step, yamlstr, tmpl, ovrwt={}):
     '''Expand a template string according to the yaml variables augmented with
 information with steps.'''
+
     variables = get_vars(yamlstr)
     variables.update(ovrwt)
     if 'hosts' in variables:
-        variables['hosts'] = generate_dict(variables['hosts'])
+        variables['hosts'] = generate.generate_dict(variables['hosts'], '=')
     validate(variables)
     variables['step'] = step
     reinject(variables)
